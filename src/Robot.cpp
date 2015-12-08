213c81bd76503e56aa1e430c440ef387c5aa888c
Jameel Jiwani <jameeljiwani@gmail.com>
2015-12-08 15:16:28 -0500

    Drive train example
    Using 1 joystick with 2 motors. Very basic example to be used as a
    template

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,39 +1,40 @@
 #include "WPILib.h"
 
-class Robot: public IterativeRobot
+/**
+ * This is a demo program showing the use of the RobotDrive class.
+ * The SampleRobot class is the base of a robot application that will automatically call your
+ * Autonomous and OperatorControl methods at the right time as controlled by the switches on
+ * the driver station or the field controls.
+ *
+ * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
+ * don't. Unless you know what you are doing, complex code will be much more difficult under
+ * this system. Use IterativeRobot or Command-Based instead if you're new.
+ */
+class Robot: public SampleRobot
 {
-private:
-	LiveWindow *lw;
+	RobotDrive myRobot; // robot drive system
+	Joystick stick; // only joystick
 
-	void RobotInit()
+public:
+	Robot() :
+			myRobot(1, 2),	// initialize the RobotDrive to use motor controllers on ports 1 and 2. It is very important that you choose the right ports
+			stick(0)  //This is the USB port that the joystick is in
 	{
-		lw = LiveWindow::GetInstance();
+		myRobot.SetExpiration(0.1);
 	}
 
-	void AutonomousInit()
+	/**
+	 * Runs the motors with arcade steering.
+	 */
+	void OperatorControl()
 	{
-
+		while (IsOperatorControl() && IsEnabled())
+		{
+			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+			Wait(0.005);				// wait for a motor update time
+		}
 	}
 
-	void AutonomousPeriodic()
-	{
-
-	}
-
-	void TeleopInit()
-	{
-
-	}
-
-	void TeleopPeriodic()
-	{
-
-	}
-
-	void TestPeriodic()
-	{
-		lw->Run();
-	}
 };
 
 START_ROBOT_CLASS(Robot);
